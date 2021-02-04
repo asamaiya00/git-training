7f0824f1b0ba2c0282559876275f52ca4d068911
asamaiya00 <asamaiya00@gmail.com>
2021-02-05 00:11:12 +0530

    fix: updated error

--- math.py
+++ math.py
@@ -1,11 +1,11 @@
 def subtract(a,b):
     if a<b:
-        return "Error"
+        return "Error: Negative value"
     return a-b
 
 def divide(a,b):
     if b==0:
-        return "Error"
+        return "Error: Division by zero"
     return a/b
 
 def square(a):
